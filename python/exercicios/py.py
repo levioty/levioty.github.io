4b80d5decb836aca557656a1b1fd232f0c8f79f9
levioty <113392896+levioty@users.noreply.github.com>
2024-10-11 21:56:51 -0300

    Create py.py

---
+++ python/exercicios/py.py
@@ -0,0 +1,38 @@
+# Construa um Algoritmo que, para um grupo de 50 valores inteiros, determine:
+
+#             a) A soma dos números positivos;
+
+#             b) A quantidade de valores negativos;
+
+# soma = 0
+# contneg = 0
+# i = 1
+# while i <=5:
+#     num = int(input('informe um numero '))
+#     if num > 0:
+#         print('positivo')
+#         soma = soma + num
+#     elif num < 0:
+#         print('negativo')
+#         contneg = contneg +1
+#     i += 1
+# print ('negativo = ', contneg)
+# print ('soma = ', soma)
+
+
+# Faça um algoritmo que imprima todos os números pares compreendidos entre 85 e 907. O algoritmo deve também calcular a soma destes valores.
+
+
+
+# num = 86
+# while num <907:
+#     print(num)
+#     num += 2
+
+
+# i = 85
+# while i<907:
+#     if i%2 ==0:
+#         print(i)
+#     i +=1
+
