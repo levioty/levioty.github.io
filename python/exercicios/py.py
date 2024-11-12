40ab0fa05e7486dfd98514fe3a351da7346fa65d
levioty <113392896+levioty@users.noreply.github.com>
2024-11-11 21:13:35 -0300

    Update py.py

--- python/exercicios/py.py
+++ python/exercicios/py.py
@@ -19,6 +19,7 @@
 # print ('negativo = ', contneg)
 # print ('soma = ', soma)
 
+# FOOOORRRRRRRRRRRR
 
 # Faça um algoritmo que imprima todos os números pares compreendidos entre 85 e 907. O algoritmo deve também calcular a soma destes valores.
 
@@ -35,4 +36,47 @@
 #     if i%2 ==0:
 #         print(i)
 #     i +=1
+
+
+#Escreva um programa que pergunte ao usuário um número e após, imprima na tela a soma total de 1 até o número lido. Exemplo: 5: 1 + 2 + 3 + 4 + 5 = 15
+        
+#numero = int(input("informe um número"))
+#soma = 0
+#for i in range (1, numero+1):
+#    print(i)
+#    soma = soma + i
+#print("A soma dos termos é" ,soma)
+
+#Faça um programa que peça dois números, base e expoente, calcule e imprima o primeiro número elevado ao segundo número. Não utilize a função de potência da linguagem.
+
+#base = int(input("informe a base"))
+#expoente = int(input("informe o expoente"))
+#potencia = 1
+#for i in range (expoente):
+#    potencia = potencia*base
+#print(potencia)  
+
+#Faça um programa para calcular e imprimir a soma dos cubos dos números pares compreendidos entre A e B (B > A). A e B são lidos pelo teclado.
+
+#a = int(input("informe um número"))
+#b = int(input("informe um número"))
+#soma = 0
+#if a>b:
+#    a,b = b,a
+#for i in range(a+1,b):
+#    if i%2==0:
+#        soma = soma + i**3
+#print(soma)
+
+
+
+
+
+
+
+
+
+
+
+
 
